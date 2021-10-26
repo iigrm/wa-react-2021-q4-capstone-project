75e01e70320f2a5995eb44b96372510f58ff54c7
Ildefonso Geronimo Ramos <ildefonso.geronimo@gmail.com>
2021-10-26 15:06:53 -0500

    Added custom hook to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
 import { Router } from "./components/Router/Router";
 
+import { useFeaturedBanners } from "./utils/hooks/useFeaturedBanners";
+
 function App() {
+  const { data, isLoading } = useFeaturedBanners();
+  console.log(data, isLoading);
+
   return <Router />;
 }
 
